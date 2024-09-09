57e700b157479cd1172e6bae29a17755522d1f1f
Alirizo <adobkin@croc.ru>
2024-09-09 15:21:24 +0500

    fixed bugs

--- medical_back/core/admin.py
+++ medical_back/core/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser, Doctor, Patient, Medication, Procedure
+from .models import CustomUser, Doctor, Patient, Medication, Procedure, Appointment, Anamesis
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
     search_fields = ('email', 'first_name', 'last_name')
+
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
@@ -12,11 +14,13 @@ class DoctorAdmin(admin.ModelAdmin):
     search_fields = ('user__email', 'user__last_name', 'user__first_name')
     list_filter = ('birth_date',)
 
+
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'middle_name', 'phone_number', 'doctor')
     search_fields = ('last_name', 'first_name', 'phone_number', 'doctor__user__email')
     list_filter = ('doctor',)
+
 
 @admin.register(Medication)
 class MedicationAdmin(admin.ModelAdmin):
@@ -24,8 +28,23 @@ class MedicationAdmin(admin.ModelAdmin):
     search_fields = ('name', 'doctor__user__email')
     list_filter = ('doctor',)
 
+
 @admin.register(Procedure)
 class ProcedureAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'patient', 'doctor')
     search_fields = ('name', 'patient__last_name', 'doctor__user__email')
     list_filter = ('doctor', 'date', 'patient')
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'name', 'date', 'time_from', 'time_to', 'comment',)
+    search_fields = ('name', 'patient__last_name', 'doctor__user__email')
+    list_filter = ('doctor', 'date', 'patient')
+
+
+@admin.register(Anamesis)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'name', 'description',)
+    search_fields = ('name', 'doctor__user__email')
+    list_filter = ('doctor', 'name')
--- medical_back/medical_back/settings.py
+++ medical_back/medical_back/settings.py
@@ -122,10 +122,10 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),  # Токен доступа действует год
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),  # Токен обновления действует год
+    'ROTATE_REFRESH_TOKENS': False,  # Не вращать токены
+    'BLACKLIST_AFTER_ROTATION': True,  # Включить список отзыва токенов
 }
 
 # Internationalization

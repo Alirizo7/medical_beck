bac6e6b1dec865b9a6ff5bd9db896d0d26c95bcb
Alirizo <adobkin@croc.ru>
2024-12-18 22:09:12 +0500

    delete photo

--- medical_back/medical_back/urls.py
+++ medical_back/medical_back/urls.py
@@ -19,16 +19,8 @@ from django.urls import path, include
 
 from core.views import SupportView
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('support/', SupportView, name='support'),
     path('', include('core.urls'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += staticfiles_urlpatterns()

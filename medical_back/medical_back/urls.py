13fde85dfc358e01d0e31331bf4ec02f4b4efd94
Muxammad <142990649+Muxammad1106@users.noreply.github.com>
2024-12-19 03:11:32 +0500

    Update urls.py
    add static config

--- medical_back/medical_back/urls.py
+++ medical_back/medical_back/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path('support/', SupportView, name='support'),
     path('', include('core.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()  # Ensure this line is included
